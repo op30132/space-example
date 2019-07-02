898c3a62776ce2a1e32ce870ecbead76cbaae2d1
op30132 <op30132@gmail.com>
2019-07-03 00:49:26 +0800

    edit mail

--- src/app/pages/member/components/edit-member/edit-member.component.ts
+++ src/app/pages/member/components/edit-member/edit-member.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, createPlatformFactory } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MemberService } from '../../services/member.service';
-import { switchMap, tap,map } from 'rxjs/operators';
+import { switchMap, tap, map } from 'rxjs/operators';
 
 import { Member } from 'src/app/shared/models/member.model';
 import { Observable } from 'rxjs';
-import {  FormGroup, FormControl  } from '@angular/forms';
+import { FormGroup, FormControl, Validator, FormBuilder, Validators } from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-edit-member',
@@ -17,32 +18,50 @@ export class EditMemberComponent implements OnInit {
   member$: Observable<Member>;
 
   profileForm: FormGroup;
-
-  constructor(private memberService: MemberService, private route: ActivatedRoute) {
-    this.profileForm = new FormGroup({
-      account: new FormControl(''),
-      status: new FormControl(''),
-      email: new FormControl(''),
-      name: new FormControl(''),
-      contactMobileTel: new FormControl(''),
-      createDate: new FormControl(''),
-      lastModifyDate: new FormControl(''),
-    });
-    // this.profileForm.controls['status'].setValue('Y', {onlySelf: true});
+  emailMsg: string = "";
+  passwordEditStatus: boolean = false;
+  constructor(private memberService: MemberService, private route: ActivatedRoute, private fb: FormBuilder) {
+    this.createForm();
   }
 
   ngOnInit() {
     this.getMember();
   }
+  createForm() {
+    this.profileForm = this.fb.group({
+      account: [null, [Validators.required]],
+      status: [null, [Validators.required]],
+      email: [null, [Validators.required]],
+      name: [null, [Validators.required]],
+      contactMobileTel: [null],
+      createDate: [null],
+      lastModifyDate: [null],
+    });
+    // this.profileForm.controls['status'].setValue('Y', {onlySelf: true});
+  }
 
-  getMember(){
+  getMember() {
     this.member$ = this.memberService
       .getMemberById(this.route.snapshot.params.id)
       .pipe(
         tap(user => this.profileForm.patchValue(user))
       );
   }
-  onSubmit(){
-
+  onSubmit() {
+    console.log(this.profileForm.value)
+    this.memberService
+      .updateMember(this.profileForm.value)
   }
+  checkEmail() {
+    if (this.profileForm.value.email) {
+      var pattern = /^[0-9?A-z0-9?]+(\.)?[0-9?A-z0-9?]+@[A-z]+\.[A-z]{3}.?[A-z]{0,3}$/i;
+      if (!pattern.test(this.profileForm.value.email)) {
+        this.emailMsg = "信箱格式不符"
+      }
+    }
+  }
+  passwordEdit() {
+    this.passwordEditStatus = true
+  }
+
 }
